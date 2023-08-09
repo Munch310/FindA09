55c9ed91ec51650d2a8f67690352065bba972210
Munch310 <ghkdlsi@naver.com>
2023-08-10 08:55:31 +0900

    Update StageSelectBtn
    각 스테이지 클리어하지 못하면 버튼 클릭할 수 없게 수정했습니다!

--- FindA09/Assets/Scripts/StageSelectBtn.cs
+++ FindA09/Assets/Scripts/StageSelectBtn.cs
@@ -2,40 +2,38 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class StageSelectBtn : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Button[] stageButtons; // 스테이지 버튼 배열
+    int currentClearedStage = Global.Instance.CurrentStage;
+
     void Start()
     {
+        // 설정하실 때, 각 StageSelectBtn1~3까지 Script 넣어주시고
+        // StageSelectBtn(Script)에서 Stage Button 3, Element0~2까지 각 버튼 다 넣어주시면됩니다!
+        // 각 스테이지 버튼의 상태 설정
+        for (int i = 0; i < stageButtons.Length; i++)
+        {
+            // i + 1이 클리어한 스테이지보다 작거나 같으면 잠금 해제
+            stageButtons[i].interactable = i + 1 <= currentClearedStage + 1;
+        }
 
-    }
-
-
-    // Update is called once per frame
-    void Update()
-    {
-
+        // 마지막 스테이지 이후의 버튼 비활성화
+        for (int i = currentClearedStage + 1; i < stageButtons.Length; i++)
+        {
+            stageButtons[i].interactable = false;
+        }
     }
 
     public void SetStage(int stage)
     {
-        // 현재 클리어한 스테이지 가져오기
-        int currentClearedStage = Global.Instance.CurrentStage;
-
-        if (currentClearedStage >= stage)
+        // 현재 클리어한 스테이지 다음 스테이지를 선택한 경우
+        if (currentClearedStage + 1 >= stage)
         {
-            // 현재 클리어한 스테이지가 선택한 스테이지보다 크거나 같으면 실행
             Global.Instance.CurrentStage = stage;
             SceneManager.LoadScene("MainScene");
         }
-        else
-        {
-            // 현재 클리어한 스테이지보다 더 높은 스테이지를 선택한 경우, 실행하지 않음
-            Debug.Log("클리어X");
-        }
     }
-
-
 }
-
