afe38f6105d847d6f57b0fba0eb9bfd48d833059
Munch310 <ghkdlsi@naver.com>
2023-08-09 23:24:03 +0900

    Add StageSelectBtn
    스테이지에 따라 스테이지 잠금 기능을 설정했습니다.

--- FindA09/Assets/Scripts/StageSelectBtn.cs
+++ FindA09/Assets/Scripts/StageSelectBtn.cs
@@ -8,19 +8,34 @@ public class StageSelectBtn : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void SetStage(int stage)
     {
-        Global.Instance.CurrentStage = stage;
-        SceneManager.LoadScene("MainScene");
+        // 현재 클리어한 스테이지 가져오기
+        int currentClearedStage = Global.Instance.CurrentStage;
+
+        if (currentClearedStage >= stage)
+        {
+            // 현재 클리어한 스테이지가 선택한 스테이지보다 크거나 같으면 실행
+            Global.Instance.CurrentStage = stage;
+            SceneManager.LoadScene("MainScene");
+        }
+        else
+        {
+            // 현재 클리어한 스테이지보다 더 높은 스테이지를 선택한 경우, 실행하지 않음
+            Debug.Log("클리어X");
+        }
     }
 
+
 }
+
